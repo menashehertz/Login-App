681e39b2b44dd5291fea42241fe4190a712f121b
Steven Hertz <hertzsteven@icloud.com>
2019-11-26 15:31:23 -0500

    Initial Commit

---
+++ Login App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login App
+//
+//  Created by Steven Hertz on 11/26/19.
+//  Copyright © 2019 DevelopItSolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
